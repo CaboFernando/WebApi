4adc4dae5a01394f83afcf6c4f66a130d5aef229
CbFernando <carlos.santos@retornar.org>
2020-03-12 11:12:19 -0300

    Creating Settings class for serve a key

---
+++ WebApi/WebApi/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public static class Settings
+    {
+        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+    }
+}
