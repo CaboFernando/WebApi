f24b538f89be8059a38042fe64667bea9fd322d5
CaboFernando <carlos.santos@retornar.org>
2020-03-12 16:26:42 -0300

    Altering User class for  data base access and creating class and interface of comunication with data base

--- WebApi/WebApi/Models/User.cs
+++ WebApi/WebApi/Models/User.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace WebApi.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        //Not using mongodb
+        //public int Id { get; set; }
+        //public string Username { get; set; }
+        //public string Password { get; set; }
+        //public string Role { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
         public string Username { get; set; }
+
         public string Password { get; set; }
+
         public string Role { get; set; }
     }
 }
---
+++ WebApi/WebApi/Models/UserstoreDatabaseSettings.cs
@@ -0,0 +1,17 @@
+﻿
+namespace WebApi.Models
+{
+    public class UserstoreDatabaseSettings : IUserstoreDatabaseSettings
+    {
+        public string UsersCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IUserstoreDatabaseSettings
+    {
+        string UsersCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
