0d8249d82ea3a830ebc9982ce316c9207ed488ea
CbFernando <carlos.santos@retornar.org>
2020-03-12 11:09:01 -0300

    Creating UserRepository for emulate a database access

---
+++ WebApi/WebApi/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "Andre", Password = "Andre123", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "Carlos", Password = "Carlos123", Role = "employee" });
+            return users.Where(x => x.Username.ToLower() == username && x.Password == password).FirstOrDefault();
+        }
+    }
+}
